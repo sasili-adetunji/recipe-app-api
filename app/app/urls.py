c77bcfa1480de5dc311683fd92f14f9d72c162bf
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2023-02-12 21:42:32 +0100

    add drf spectacualr

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectaculaSwaggerView
+)
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/', SpectaculaSwaggerView.as_view(url_name='api-schema'), name='api-docs')
 ]
